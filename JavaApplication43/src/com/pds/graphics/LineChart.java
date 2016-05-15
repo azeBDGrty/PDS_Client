564fb89dc844a21ae8f86d2ec67ccffe49b55b71
Nodaro <Nodaro@PC-Nodaro>
2016-05-15 15:21:05 +0200

    Commit Nodaro

--- JavaApplication43/src/com/pds/graphics/LineChart.java
+++ JavaApplication43/src/com/pds/graphics/LineChart.java
@@ -22,6 +22,7 @@ import javax.swing.SwingUtilities;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.XYToolTipGenerator;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
@@ -39,7 +40,7 @@ import org.jfree.ui.RefineryUtilities;
  */
 public class LineChart extends ApplicationFrame {
     
-        private static CalculPret calculPret;
+    private static CalculPret calculPret;
     private static SimulationPret simPret;
     
     public LineChart() throws SQLException {
@@ -67,7 +68,12 @@ public class LineChart extends ApplicationFrame {
                 xAxisLabel, yAxisLabel, dataset);
         
         XYPlot plot = chart.getXYPlot();
+        ChartPanel chartpanel=new ChartPanel (chart);
+        chartpanel.setInitialDelay(100);
+        chartpanel.setDismissDelay(100);
+        chartpanel.setReshowDelay(100);
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        
         // sets paint color for each series
         renderer.setSeriesPaint(0, Color.RED);
         renderer.setSeriesPaint(1, Color.GREEN);
@@ -77,15 +83,30 @@ public class LineChart extends ApplicationFrame {
         renderer.setSeriesStroke(0, new BasicStroke(2.0f));
         renderer.setSeriesStroke(1, new BasicStroke(2.0f));
         renderer.setSeriesStroke(2, new BasicStroke(2.0f));
+        
         plot.setRenderer(renderer);
         plot.setBackgroundPaint(Color.WHITE);
         //plot.setRangeGridlinesVisible(true);
         plot.setRangeGridlinePaint(Color.BLACK);
-
+        
         plot.setDomainGridlinesVisible(true);
         plot.setDomainGridlinePaint(Color.BLACK);
+        XYToolTipGenerator xyToolTipGenerator =(XYDataset xyd, int i, int i1) -> {      //mouseover sur un point d'une courbe
+            Number x1 = dataset.getX(i, i1);
+            Number y1 = dataset.getY(i, i1);
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(String.format("<html><p style='color:#0000ff;'>Serie: '%s'</p>", dataset.getSeriesKey(i)));
+            stringBuilder.append(String.format("Mois:'%d'<br/>", x1.intValue()));
+            stringBuilder.append(String.format("Montant:'%d'", y1.intValue()));
+            stringBuilder.append("</html>");
+            return stringBuilder.toString();
+        };
+        
+        renderer.setBaseToolTipGenerator(xyToolTipGenerator);
         return new ChartPanel(chart);
     }
+    
+    
     
     private XYDataset createDataset() {
         // creates an XY dataset...
@@ -108,8 +129,9 @@ public class LineChart extends ApplicationFrame {
             series1.add(i,capAmorti.get(i) );
             series2.add(i, capRestant.get(i));
             series3.add(i, evolutionInterets.get(i));
+            
         }
-       
+        
         
         dataset.addSeries(series1);
         dataset.addSeries(series2);
@@ -131,12 +153,17 @@ public class LineChart extends ApplicationFrame {
         });
     }
     
-public static void init(int idClient) throws SQLException{
-       simPret=new SimulationPret();
-       calculPret=new CalculPret();
+    public static void init(int idClient) throws SQLException{
+        simPret=new SimulationPret();
+        calculPret=new CalculPret();
         Taux_directeur td=new Taux_directeur();
         calculPret.setTauxDirecteur(td);
         simPret.setCalcPret(calculPret);
         simPret.setAmortisationCalcPret(calculPret, idClient,td);
-   }}
+    }
+    
+    
+    
+    
+}
 
